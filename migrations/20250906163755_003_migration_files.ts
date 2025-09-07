f7f150cd3814198b15f7c2d1d10feaa008bb4bed
Farid Hidayat <farid@startapp.id>
2025-09-08 01:10:30 +0700

    Update migration script for 'files' table to allow nullable fields and change 'type' to enum with default value 'image'

--- migrations/20250906163755_003_migration_files.ts
+++ migrations/20250906163755_003_migration_files.ts
@@ -7,17 +7,14 @@ export async function up(knex: Knex): Promise<void> {
     table.string('fullpath', 1024).notNullable();
     table.string('path', 1024).notNullable();
     table.string('filename', 1024).notNullable();
-    table.string('title', 1024);
-    table.string('description', 1024);
-    table.integer('size').notNullable();
-    table.string('ext', 10).notNullable();
-    table.string('type', 255).notNullable().defaultTo('other');
-    table.string('foreign_table', 255);
-    table.string('foreign_id', 255);
+    table.string('title', 1024).nullable();
+    table.string('description', 1024).nullable();
+    table.integer('size').nullable();
+    table.string('ext', 10).nullable();
+    table.enum('type', ['image', 'video', 'audio', 'document', 'other']).notNullable().defaultTo('image');
     table.timestamp('created_at', { useTz: false }).notNullable().defaultTo(knex.fn.now());
     table.timestamp('updated_at', { useTz: false }).notNullable().defaultTo(knex.fn.now());
     table.timestamp('deleted_at', { useTz: false });
-    table.integer('order').notNullable().defaultTo(0);
   });
 }
 
