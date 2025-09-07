dd50bdada4c6aefba90aed0090281283fff6b5e7
Farid Hidayat <farid@startapp.id>
2025-09-07 14:39:19 +0700

    Add migration script to create 'religions' table with necessary fields and constraints

---
+++ migrations/20250906163784_032_migration_religions.ts
@@ -0,0 +1,18 @@
+import type { Knex } from "knex";
+
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema.createTable('religions', (table) => {
+    table.uuid('id').primary().defaultTo(knex.raw('gen_random_uuid()'));
+    table.string('name', 255).notNullable();
+    table.timestamp('created_at', { useTz: false }).notNullable().defaultTo(knex.fn.now());
+    table.timestamp('updated_at', { useTz: false }).notNullable().defaultTo(knex.fn.now());
+    table.timestamp('deleted_at', { useTz: false });
+
+    // Create indexes
+    table.index('name');
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTable('religions');
+}
