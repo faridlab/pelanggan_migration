7830cbfb12d1b5c28204c4b30ed1939b2e208383
Farid Hidayat <farid@startapp.id>
2025-09-07 14:48:14 +0700

    Add migration script to create 'reviews' table with necessary fields, constraints, and indexes

---
+++ migrations/20250906163854_102_migration_reviews.ts
@@ -0,0 +1,29 @@
+import type { Knex } from "knex";
+
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema.createTable('reviews', (table) => {
+    table.uuid('id').primary().defaultTo(knex.raw('gen_random_uuid()'));
+    table.string('foreign_table', 255).nullable();
+    table.string('foreign_id', 255).nullable();
+    table.string('scope', 255).nullable();
+    table.smallint('rate').notNullable().defaultTo(0);
+    table.text('review').nullable();
+    table.uuid('created_by').notNullable();
+    table.uuid('updated_by').nullable();
+    table.uuid('deleted_by').nullable();
+    table.timestamp('created_at', { useTz: false }).notNullable().defaultTo(knex.fn.now());
+    table.timestamp('updated_at', { useTz: false }).notNullable().defaultTo(knex.fn.now());
+    table.timestamp('deleted_at', { useTz: false }).nullable();
+
+    // Create indexes
+    table.index('foreign_table');
+    table.index('foreign_id');
+    table.index('scope');
+    table.index('rate');
+    table.index('created_by');
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTable('reviews');
+}
