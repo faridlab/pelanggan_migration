ab6ba5f16b45ced0649d44ea9e90626da1d296d9
Farid Hidayat <farid@startapp.id>
2025-09-07 14:39:12 +0700

    Add migration script to create 'banks' table with necessary fields and constraints

---
+++ migrations/20250906163783_031_migration_banks.ts
@@ -0,0 +1,21 @@
+import type { Knex } from "knex";
+
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema.createTable('banks', (table) => {
+    table.uuid('id').primary().defaultTo(knex.raw('gen_random_uuid()'));
+    table.string('name', 255).notNullable();
+    table.string('short_name', 255).nullable();
+    table.string('code', 3).nullable();
+    table.timestamp('created_at', { useTz: false }).notNullable().defaultTo(knex.fn.now());
+    table.timestamp('updated_at', { useTz: false }).notNullable().defaultTo(knex.fn.now());
+    table.timestamp('deleted_at', { useTz: false });
+
+    // Create indexes
+    table.index('name');
+    table.index('code');
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTable('banks');
+}
