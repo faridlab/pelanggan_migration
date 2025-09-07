b009881ef1b1a6dc5714fcf2bd8a8bb323ac3912
Farid Hidayat <farid@startapp.id>
2025-09-08 01:15:09 +0700

    Update migration script for 'role_has_permissions' table to make 'updated_at' field not nullable with default value

--- migrations/20250906163759_007_migration_role_has_permissions.ts
+++ migrations/20250906163759_007_migration_role_has_permissions.ts
@@ -5,7 +5,7 @@ export async function up(knex: Knex): Promise<void> {
     table.uuid('permission_id').notNullable();
     table.uuid('role_id').notNullable();
     table.timestamp('created_at', { useTz: false }).notNullable().defaultTo(knex.fn.now());
-    table.timestamp('updated_at', { useTz: false });
+    table.timestamp('updated_at', { useTz: false }).notNullable().defaultTo(knex.fn.now());
     table.timestamp('deleted_at', { useTz: false });
 
     // Create composite primary key
