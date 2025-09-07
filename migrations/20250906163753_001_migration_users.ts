78962004778d783c6d5560faba89a800f47f7b80
Farid Hidayat <farid@startapp.id>
2025-09-08 01:05:57 +0700

    Add migration script to update 'users' table with additional fields: middle_name, dob, phone, gender, and updated status definition

--- migrations/20250906163753_001_migration_users.ts
+++ migrations/20250906163753_001_migration_users.ts
@@ -4,19 +4,20 @@ export async function up(knex: Knex): Promise<void> {
   return knex.schema.createTable('users', (table) => {
     table.uuid('id').primary().defaultTo(knex.raw('gen_random_uuid()'));
     table.string('first_name', 255).notNullable();
+    table.string('middle_name', 255).nullable();
     table.string('last_name', 255).notNullable();
     table.string('username', 255).notNullable().unique();
     table.string('email', 255).notNullable().unique();
     table.timestamp('email_verified_at', { useTz: false });
     table.string('password', 255).notNullable();
-    table.string('status', 255).notNullable().defaultTo('inactive');
-    table.string('remember_token', 100);
+    table.date('dob').nullable();
+    table.string('phone', 50).nullable();
+    table.enum('gender', ['male', 'female']).nullable();
+    table.enum('status', ['active', 'inactive']).notNullable().defaultTo('inactive');
+    table.string('remember_token', 100).nullable();
     table.timestamp('created_at', { useTz: false }).notNullable().defaultTo(knex.fn.now());
     table.timestamp('updated_at', { useTz: false }).notNullable().defaultTo(knex.fn.now());
     table.timestamp('deleted_at', { useTz: false });
-    table.date('dob');
-    table.string('phone', 25).unique();
-    table.string('gender', 255);
   });
 }
 
