4091fa378367735e3f1610422e3ded915ddc7619
Farid Hidayat <farid@startapp.id>
2025-09-07 14:29:11 +0700

    Add migration script to create 'countries' table with necessary fields and constraints

---
+++ migrations/20250906163762_010_migration_countries.ts
@@ -0,0 +1,17 @@
+import type { Knex } from "knex";
+
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema.createTable('countries', (table) => {
+    table.uuid('id').primary().defaultTo(knex.raw('gen_random_uuid()'));
+    table.string('isocode', 2).notNullable();
+    table.string('name', 255).notNullable();
+    table.integer('phonecode').notNullable();
+    table.timestamp('created_at', { useTz: false }).notNullable().defaultTo(knex.fn.now());
+    table.timestamp('updated_at', { useTz: false }).notNullable().defaultTo(knex.fn.now());
+    table.timestamp('deleted_at', { useTz: false });
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTable('countries');
+}
