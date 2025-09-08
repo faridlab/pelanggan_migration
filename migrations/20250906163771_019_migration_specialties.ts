184331d38bc0b3ccfefd9ef1cd985f1494d8da26
Farid Hidayat <farid@startapp.id>
2025-09-08 23:23:22 +0700

    Add migration script to create 'specialties' table with fields, indexes, and foreign key constraint

---
+++ migrations/20250906163771_019_migration_specialties.ts
@@ -0,0 +1,26 @@
+import type { Knex } from "knex";
+
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema.createTable('specialties', (table) => {
+    table.uuid('id').primary().defaultTo(knex.raw('gen_random_uuid()'));
+    table.uuid('industry_id').notNullable();
+    table.string('name', 255).notNullable();
+    table.timestamp('created_at', { useTz: false }).notNullable().defaultTo(knex.fn.now());
+    table.timestamp('updated_at', { useTz: false }).notNullable().defaultTo(knex.fn.now());
+    table.timestamp('deleted_at', { useTz: false });
+
+    // Create indexes
+    table.index('industry_id');
+    table.index('name');
+
+    table.foreign('industry_id', 'specialties_industry_id_foreign')
+      .references('id')
+      .inTable('industries')
+      .onDelete('RESTRICT')
+      .onUpdate('CASCADE');
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTable('specialties');
+}
