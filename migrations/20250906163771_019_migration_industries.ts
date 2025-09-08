bda459b8cd6823c6370861270da6df04b10512a1
Farid Hidayat <farid@startapp.id>
2025-09-08 23:23:12 +0700

    Add migration script to create 'industries' table with fields, indexes, and default values

--- migrations/20250906163771_019_migration_industries.ts
+++ migrations/20250906163771_019_migration_industries.ts
@@ -1,7 +1,7 @@
 import type { Knex } from "knex";
 
 export async function up(knex: Knex): Promise<void> {
-  return knex.schema.createTable('organization_industries', (table) => {
+  return knex.schema.createTable('industries', (table) => {
     table.uuid('id').primary().defaultTo(knex.raw('gen_random_uuid()'));
     table.string('name', 255).notNullable();
     table.timestamp('created_at', { useTz: false }).notNullable().defaultTo(knex.fn.now());
@@ -14,5 +14,5 @@ export async function up(knex: Knex): Promise<void> {
 }
 
 export async function down(knex: Knex): Promise<void> {
-  return knex.schema.dropTable('organization_industries');
+  return knex.schema.dropTable('industries');
 }
