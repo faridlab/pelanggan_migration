ad705f843ddd7d2007eea1870b5b670cadcc04b1
Farid Hidayat <farid@startapp.id>
2025-09-09 20:23:46 +0700

    Refactor 'employee_taxes' migration to use enums for 'ptkp_status', 'tax_method', 'tax_salary', and 'tax_status' fields with predefined values and set default values for each.

--- migrations/20250906163793_041_migration_employee_taxes.ts
+++ migrations/20250906163793_041_migration_employee_taxes.ts
@@ -1,15 +1,48 @@
 import type { Knex } from "knex";
 
 export async function up(knex: Knex): Promise<void> {
+  const ptkp_status = ['TK/0', 'TK/1', 'TK/2', 'TK/3', 'K/0', 'K/1', 'K/2', 'K/3'];
+  const tax_methods = ['gross', 'gross-up', 'netto'];
+  const tax_salary = ['taxable', 'non-taxable'];
+  const tax_statuses = [
+      'EMP-PRM',
+      'EMP-XPRM',
+      'XEMP-CTN',
+      'XEMP-XCTN',
+      'XPT',
+      'XPT-DN',
+      'XPRT-CTN',
+      'XPRT-XCTN',
+      'COMM',
+      'XPRT-CTN01',
+      'FRL',
+      'XEMP-XCTN01',
+  ];
+
+  // TAX STATUS:
+  // ===========
+  // EMP-PRM: Pegawai tetap
+  // EMP-XPRM: Pegawai tidak tetap
+  // XEMP-CTN: Bukan pegawai yang bersifat berkesinambungan
+  // XEMP-XCTN: Bukan pegawai yang tidak bersifat berkesinambungan
+  // XPT: Ekspatriat
+  // XPT-DN: Ekspatriat dalam negeri
+  // XPRT-CTN: Tenaga ahli yang bersifat berkesinambungan
+  // XPRT-XCTN: Tenaga ahli yang tidak bersifat berkesinambungan
+  // COMM: Dewan komisaris
+  // XPRT-CTN01: Tenaga ahli yang bersifat berkesinambungan >1 PK
+  // FRL: Tenaga kerja lepas
+  // XEMP-XCTN01: Bukan pegawai yang bersifat berkesinambungan >1 PK
+
   return knex.schema.createTable('employee_taxes', (table) => {
     table.uuid('id').primary().defaultTo(knex.raw('gen_random_uuid()'));
     table.uuid('employee_id').notNullable();
     table.string('npwp_number', 255).notNullable();
-    table.string('ptkp_status', 255).notNullable();
-    table.string('tax_method', 255).notNullable();
-    table.string('tax_salary', 255).notNullable();
+    table.enum('ptkp_status', ptkp_status).notNullable().defaultTo('TK/0');
+    table.enum('tax_method', tax_methods).notNullable().defaultTo('gross');
+    table.enum('tax_salary', tax_salary).notNullable().defaultTo('taxable');
     table.date('taxable_date').notNullable();
-    table.string('tax_status', 255).notNullable();
+    table.enum('tax_status', tax_statuses).notNullable().defaultTo('EMP-PRM');
     table.integer('beginning_netto').nullable();
     table.integer('pph21_paid').nullable();
     table.timestamp('created_at', { useTz: false }).notNullable().defaultTo(knex.fn.now());
