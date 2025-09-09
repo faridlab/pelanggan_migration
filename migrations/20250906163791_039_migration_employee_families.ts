e8f5f6b38d34bd29aba01498807976f76a53b327
Farid Hidayat <farid@startapp.id>
2025-09-09 20:23:25 +0700

    Refactor 'employee_families' migration to use enums for 'relationship' and 'gender' fields with predefined values, and add nullable fields for 'relationship_other', 'birth_place', and 'birth_date'.

--- migrations/20250906163791_039_migration_employee_families.ts
+++ migrations/20250906163791_039_migration_employee_families.ts
@@ -1,11 +1,40 @@
 import type { Knex } from "knex";
 
 export async function up(knex: Knex): Promise<void> {
+  const RELATIONSHIPS = [
+    'grandfather',
+    'grandmother',
+    'father',
+    'mother',
+    'uncle',
+    'aunt',
+    'sibling',
+    'cousin',
+    'niece',
+    'nephew',
+    'spouse',
+    'husband',
+    'wife',
+    'child',
+    'grandchild',
+    'son-in-law',
+    'daughter-in-law',
+    'co-parents-in-law',
+    'brother-in-law',
+    'sister-in-law',
+    'other',
+  ];
+
+  const GENDERS = ['male', 'female', 'other'];
   return knex.schema.createTable('employee_families', (table) => {
     table.uuid('id').primary().defaultTo(knex.raw('gen_random_uuid()'));
     table.uuid('employee_id').notNullable();
     table.string('name', 255).notNullable();
-    table.string('relationship', 255).notNullable();
+    table.enum('relationship', RELATIONSHIPS).notNullable().defaultTo('spouse');
+    table.string('relationship_other', 255).nullable();
+    table.string('birth_place', 255).nullable();
+    table.date('birth_date').nullable();
+    table.enum('gender', GENDERS).notNullable().defaultTo('male');
     table.timestamp('created_at', { useTz: false }).notNullable().defaultTo(knex.fn.now());
     table.timestamp('updated_at', { useTz: false }).notNullable().defaultTo(knex.fn.now());
     table.timestamp('deleted_at', { useTz: false });
