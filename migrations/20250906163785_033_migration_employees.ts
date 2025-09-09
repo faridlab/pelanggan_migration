12450ee1f55bc2bf6d666b92d99fd1d909a15317
Farid Hidayat <farid@startapp.id>
2025-09-09 20:23:12 +0700

    Refactor 'employees' migration to use enums for 'gender', 'martial_status', and 'blood_type' fields with predefined values.

--- migrations/20250906163785_033_migration_employees.ts
+++ migrations/20250906163785_033_migration_employees.ts
@@ -1,6 +1,9 @@
 import type { Knex } from "knex";
 
 export async function up(knex: Knex): Promise<void> {
+  const GENDERS = ['male', 'female', 'other'];
+  const MARITAL_STATUSES = ['single', 'married', 'widowed', 'divorced', 'separated'];
+  const BLOOD_TYPES = ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-', 'other'];
   return knex.schema.createTable('employees', (table) => {
     table.uuid('id').primary().defaultTo(knex.raw('gen_random_uuid()'));
     table.string('employee_number', 255).notNullable();
@@ -11,9 +14,9 @@ export async function up(knex: Knex): Promise<void> {
     table.string('phone', 255).nullable();
     table.string('birth_place', 255).notNullable();
     table.date('birth_date').notNullable();
-    table.string('gender', 255).notNullable();
-    table.string('martial_status', 255).notNullable();
-    table.string('blood_type', 255).nullable();
+    table.enum('gender', GENDERS).notNullable().defaultTo('male');
+    table.enum('martial_status', MARITAL_STATUSES).notNullable().defaultTo('single');
+    table.enum('blood_type', BLOOD_TYPES).nullable();
     table.uuid('religion_id').notNullable();
     table.timestamp('created_at', { useTz: false }).notNullable().defaultTo(knex.fn.now());
     table.timestamp('updated_at', { useTz: false }).notNullable().defaultTo(knex.fn.now());
