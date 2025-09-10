8f7e99d33694094f089eb314203c3b025e2d54ad
Farid Hidayat <farid@startapp.id>
2025-09-10 23:41:38 +0700

    Refactor 'quotations' migration to use enums for 'type', 'risk_level', and 'status' fields with predefined values. Update default values accordingly and add comments for clarity.

--- migrations/20250906163825_073_migration_quotations.ts
+++ migrations/20250906163825_073_migration_quotations.ts
@@ -1,19 +1,22 @@
 import type { Knex } from "knex";
 
 export async function up(knex: Knex): Promise<void> {
+  const types = ['proposal', 'quote', 'estimate', 'other'];
+  const risk_levels = ['low', 'normal', 'medium', 'high', 'critical'];
+  const statuses = ['active', 'inactive', 'draft'];
   return knex.schema.createTable('quotations', (table) => {
     table.uuid('id').primary().defaultTo(knex.raw('gen_random_uuid()'));
-    table.string('type', 255).notNullable().defaultTo('proposal');
+    table.enum('type', types).notNullable().defaultTo('proposal').comment('proposal for lead, and estimate for client');
     table.uuid('industry_id').notNullable();
     table.decimal('base_price', 15, 2).notNullable();
-    table.string('risk_level', 255).notNullable().defaultTo('normal');
-    table.string('code_number', 255).notNullable();
+    table.enum('risk_level', risk_levels).notNullable().defaultTo('normal').comment('normal: {base_price} x 1, medium: {base_price} x 3, high: {base_price} x 5');
+    table.string('code_number', 255).notNullable().comment('ex: 001/{QUO|EST|INV}/{PROJECT CODE}/IV/2023');
     table.uuid('contact_id').notNullable();
     table.date('valid_from').notNullable();
     table.date('valid_end').notNullable();
     table.text('description').notNullable();
     table.json('data').nullable();
-    table.string('status', 255).notNullable().defaultTo('active');
+    table.enum('status', statuses).notNullable().defaultTo('draft');
     table.boolean('is_template').notNullable().defaultTo(false);
     table.timestamp('created_at', { useTz: false }).notNullable().defaultTo(knex.fn.now());
     table.timestamp('updated_at', { useTz: false }).notNullable().defaultTo(knex.fn.now());
