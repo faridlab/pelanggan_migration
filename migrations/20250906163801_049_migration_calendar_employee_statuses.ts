240030d9011a308e90c3f03ad6b53c1ccb48b8ff
Farid Hidayat <farid@startapp.id>
2025-09-10 22:51:57 +0700

    Refactor 'calendar_employee_statuses' migration to use enums for 'employment_status' with predefined values and set default to 'permanent'.

--- migrations/20250906163801_049_migration_calendar_employee_statuses.ts
+++ migrations/20250906163801_049_migration_calendar_employee_statuses.ts
@@ -1,10 +1,11 @@
 import type { Knex } from "knex";
 
 export async function up(knex: Knex): Promise<void> {
+  const employment_statuses = ['permanent', 'contract', 'probation', 'assosiate', 'intern', 'part-time', 'promoted', 'all'];
   return knex.schema.createTable('calendar_employee_statuses', (table) => {
     table.uuid('id').primary().defaultTo(knex.raw('gen_random_uuid()'));
     table.uuid('calendar_id').notNullable();
-    table.string('employment_status', 255).notNullable();
+    table.enum('employment_status', employment_statuses).notNullable().defaultTo('permanent');
     table.timestamp('created_at', { useTz: false }).notNullable().defaultTo(knex.fn.now());
     table.timestamp('updated_at', { useTz: false }).notNullable().defaultTo(knex.fn.now());
     table.timestamp('deleted_at', { useTz: false });
