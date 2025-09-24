d73effccbf74397ea216dd5064d4166efc272884
Farid Hidayat <farid@startapp.id>
2025-09-24 20:57:24 +0700

    Refactor 'showcases' migration to use enums for 'status' field with predefined values, add 'order' field with default value of 0, and remove 'slug' index.

--- migrations/20250906163834_082_migration_showcases.ts
+++ migrations/20250906163834_082_migration_showcases.ts
@@ -1,23 +1,20 @@
 import type { Knex } from "knex";
 
 export async function up(knex: Knex): Promise<void> {
+  const STATUS = ['active', 'inactive'];
   return knex.schema.createTable('showcases', (table) => {
     table.uuid('id').primary().defaultTo(knex.raw('gen_random_uuid()'));
     table.string('name', 255).notNullable();
-    table.string('slug', 255).nullable();
     table.string('short_desc', 500).nullable();
     table.text('desc').nullable();
-    table.string('status', 255).notNullable().defaultTo('active');
+    table.enum('status', STATUS).notNullable().defaultTo('active');
+    table.integer('order').nullable().defaultTo(0);
     table.timestamp('created_at', { useTz: false }).notNullable().defaultTo(knex.fn.now());
     table.timestamp('updated_at', { useTz: false }).notNullable().defaultTo(knex.fn.now());
     table.timestamp('deleted_at', { useTz: false }).nullable();
-    table.integer('order').nullable();
 
     // Create indexes
     table.index('name');
-    table.index('slug');
-    table.index('status');
-    table.index('order');
   });
 }
 
