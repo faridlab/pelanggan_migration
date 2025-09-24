44bb00320a988f8ceccac4dbd0fbfd4867ae7d10
Farid Hidayat <farid@startapp.id>
2025-09-24 20:57:14 +0700

    Refactor 'comments' migration to remove unnecessary foreign key fields, make 'created_by' nullable, and add 'reply_comment_id' for nested comments. Update indexes and foreign key constraints accordingly.

--- migrations/20250906163833_081_migration_comments.ts
+++ migrations/20250906163833_081_migration_comments.ts
@@ -3,27 +3,23 @@ import type { Knex } from "knex";
 export async function up(knex: Knex): Promise<void> {
   return knex.schema.createTable('comments', (table) => {
     table.uuid('id').primary().defaultTo(knex.raw('gen_random_uuid()'));
-    table.string('foreign_table', 255).nullable();
-    table.string('foreign_id', 255).nullable();
     table.string('scope', 255).nullable();
+    table.uuid('reply_comment_id').nullable();
     table.text('comment').notNullable();
-    table.uuid('created_by').notNullable();
+    table.uuid('created_by').nullable();
     table.uuid('updated_by').nullable();
     table.uuid('deleted_by').nullable();
     table.timestamp('created_at', { useTz: false }).notNullable().defaultTo(knex.fn.now());
     table.timestamp('updated_at', { useTz: false }).notNullable().defaultTo(knex.fn.now());
     table.timestamp('deleted_at', { useTz: false }).nullable();
-    table.uuid('comment_id').nullable();
 
     // Create indexes
-    table.index('foreign_table');
-    table.index('foreign_id');
     table.index('scope');
     table.index('created_by');
-    table.index('comment_id');
+    table.index('reply_comment_id');
 
     // Create foreign key constraints
-    table.foreign('comment_id', 'comments_comment_id_foreign')
+    table.foreign('reply_comment_id', 'comments_reply_comment_id_foreign')
       .references('id')
       .inTable('comments')
       .onDelete('NO ACTION')
